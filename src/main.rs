f39c1f8540baceb75a64430156df41d316ce50ac
Kian Shepherd <Kianshepherd73@gmail.com>
2020-09-02 16:16:56 -0400

    Chapter 3 done more functionality to Vec3 class to be added later

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod vec3;
 // run with
 // cargo make all
 
@@ -5,16 +6,18 @@ fn main() {
     let image_width = 256;
     let image_height = 256;
     let mut output = format!("P3\n{} {}\n255\n", image_width, image_height);
-    let b = (0.25 * 255.99) as i32;
+
     for j in 0..image_height {
+        // progress check
         if j % ((image_height as f64 / 11.0) as i32) == 0 {
             eprintln!("{:.2}% done", (j as f64 / image_height as f64) * 100.0);
         }
+
         for i in 0..image_width {
-            let r = ((i as f64 / (image_width - 1) as f64) * 255.999) as i32;
-            let g =
-                ((((image_height - j) - 1) as f64 / (image_height - 1) as f64) * 255.999) as i32;
-            output = format!("{}\n{} {} {}", output, r, g, b);
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = (image_height - (j + 1)) as f64 / (image_height - 1) as f64;
+            let color = vec3::Vec3::new(r, g, 0.25);
+            output = format!("{}\n{}", output, color.to_string());
         }
     }
     eprintln!("\nDone.\n");
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,1 +1,67 @@
-struct Vec3 {}
+pub struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    pub fn new(_x: f64, _y: f64, _z: f64) -> Vec3 {
+        Vec3 {
+            x: _x,
+            y: _y,
+            z: _z,
+        }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+
+    pub fn z(&self) -> f64 {
+        self.z
+    }
+
+    pub fn get(&self, idx: usize) -> f64 {
+        assert!(idx <= 3);
+        match idx {
+            1 => self.x,
+            2 => self.y,
+            3 => self.z,
+            _ => panic!("invalid idx: {} for vec3", idx),
+        }
+    }
+    pub fn negate(&mut self) {
+        self.x = -self.x;
+        self.y = -self.y;
+        self.z = -self.z;
+    }
+    pub fn scale(&mut self, scale_factor: f64) {
+        self.x = self.x * scale_factor;
+        self.y = self.y * scale_factor;
+        self.z = self.z * scale_factor;
+    }
+
+    pub fn to_string(&self) -> String {
+        format!(
+            "{} {} {}",
+            (self.x * 255.999) as u32,
+            (self.y * 255.999) as u32,
+            (self.z * 255.999) as u32
+        )
+    }
+}
+
+impl std::ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn add(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
