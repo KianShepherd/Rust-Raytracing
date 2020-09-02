d77ffe1cb04cfc99901dddb30bb8d668d991786a
Kian Shepherd <Kianshepherd73@gmail.com>
2020-09-02 14:47:51 -0400

    Chapter 2 Done

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # raytracing
 A rust implimentation of Raytracing in a weekend
 https://raytracing.github.io/books/RayTracingInOneWeekend.html
+
+# Requires 
+cargo-make is used for building the project.
+though it can be manually made, the makefile simplifies the build and testing 
+process as well as opening the created image in feh to quickly view.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// run with
+// cargo make all
+
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+    let mut output = format!("P3\n{} {}\n255\n", image_width, image_height);
+    let b = (0.25 * 255.99) as i32;
+    for j in 0..image_height {
+        if j % ((image_height as f64 / 11.0) as i32) == 0 {
+            eprintln!("{:.2}% done", (j as f64 / image_height as f64) * 100.0);
+        }
+        for i in 0..image_width {
+            let r = ((i as f64 / (image_width - 1) as f64) * 255.999) as i32;
+            let g =
+                ((((image_height - j) - 1) as f64 / (image_height - 1) as f64) * 255.999) as i32;
+            output = format!("{}\n{} {} {}", output, r, g, b);
+        }
+    }
+    eprintln!("\nDone.\n");
+    println!("{}", output);
 }
