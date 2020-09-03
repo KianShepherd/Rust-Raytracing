2d7a8255996e1a45538cfa084c5634e7b156d549
Kian Shepherd <Kianshepherd73@gmail.com>
2020-09-02 23:31:34 -0400

    minor refactoring

--- src/hittables.rs
+++ src/hittables.rs
@@ -1,23 +1,22 @@
-use crate::hittable;
+use crate::hittable::{HitRecord, Hittable};
 use crate::ray::Ray;
 
-pub struct Hittables<T: hittable::Hittable> {
+pub struct Hittables<T: Hittable> {
     hittables: Vec<T>,
 }
 
 #[allow(dead_code)]
 impl<T> Hittables<T>
 where
-    T: hittable::Hittable,
+    T: Hittable,
 {
-    pub fn hit(&self, ray: Ray, t_min: f64, t_max: f64, rec: &mut hittable::HitRecord) -> bool {
+    pub fn hit(&self, ray: Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
         let mut hit_anything = false;
-        let mut temp_rec = hittable::HitRecord::new();
+        let mut temp_rec = HitRecord::new();
         let mut closest = t_max;
 
-        for hittabl in &self.hittables {
-            let hit = hittabl.hit(ray, t_min, closest, &mut temp_rec);
-            if hit {
+        for hittable in &self.hittables {
+            if hittable.hit(ray, t_min, closest, &mut temp_rec) {
                 hit_anything = true;
                 closest = temp_rec.get_t().unwrap();
                 rec.set_rec(&temp_rec);
