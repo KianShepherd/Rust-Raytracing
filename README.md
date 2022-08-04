6e96c2ddabee2cc58c8586ae06ac9d8cc092b064
Kian Shepherd <kianshepherd73@gmail.com>
2022-08-03 21:18:54 -0400

    Move to pyrays backend (#7)
    * fix
    
    * fix build
    
    * add initial python ron object parsing
    
    * add light support
    
    * add light support
    
    * add spheres
    
    * remove unneeded deps
    
    * triangle support for pyrays
    
    * fix bug
    
    * Remove image library as a dependency
    
    * fix
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    Co-authored-by: Kian Shepherd <kshepherd@kx.com>

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Rust-Raytracing
 A Rust implementation of [_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html). 
 It currently implements all functionality of the book excluding Dielectrics which are a WIP. I have also added the code 
-for ray triangle intersection as well as procedural terrain generation with triangle meshes and perlin noise height 
-mapping. Simple lighting has also been created by sending a ray back towards the light source. Multi-threading has also 
-been added and can optionally be turned off with a flag within the main function.
+for ray triangle intersection. Simple lighting has also been created by sending a ray back towards the light source. Multi-threading has also 
+been added and can optionally be turned off.
 
 
 ![Example Render1](https://github.com/KianShepherd/Rust-Raytracing/blob/master/example1.jpg?raw=true)
@@ -15,13 +14,11 @@ been added and can optionally be turned off with a flag within the main function
 ## Dependencies
 * Rand  = "*"
 * Noise = "0.7"
-* Image = "*"
 * num_cpus = "1.6"
 
 ## How to use
-It is highly recommended compiling in release mode to speed up the program if using it for larger renders. Once compiled
-simply supply the path to the .ron file within the /configs/ folder, and an image will be output in the main directory with
-the same name.
+This library is now used only as a backend for my python raytracing library `pyrays`, and no binary
+version is built.
 
 ## Features
 1. Materials 
@@ -38,12 +35,7 @@ the same name.
 4. Shapes
     * Spheres
     * Triangles (with optional back face culling)
-    * 2D Squares
-    * 3D Cube (IN PROGRESS)
-5. Procedural Terrain
-    * Height Map
-    * Colour Map
-6. Multi-Threading
+5. Multi-Threading
    * Benchmarks
      * Release mode on 8 Core CPU
      * 720p procedural gen
@@ -54,7 +46,4 @@ the same name.
          * multi-threaded: 0m : 19s
          * single thread: 1m : 51s
          * 5.8x speedup
-7. Config Files
-   * Raytracer settings loaded from a file to make creating multiple images with similar settings easy.
-   * allow specifying of settings file from command line (optionally multiple).
-8. Octree / K-tree Optimization (TODO)
+6. Octree / K-tree Optimization (TODO)
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use crate::camera::Camera;
+use crate::configuration::RaytracerScene;
 use crate::configuration::RaytracerSettings;
+use crate::configuration::RonObject;
 use crate::hittable::Hittable;
 use crate::hittables::Hittables;
 use crate::sphere::Sphere;
+use crate::triangle::Triangle;
 use crate::vec3::Vec3;
-use image::{ImageBuffer, Rgb, RgbImage};
+use material::Material;
 use num_cpus;
 use rand::Rng;
 use std::sync::{Arc, Mutex};
@@ -12,17 +15,12 @@ use std::time::Instant;
 use std::{env, thread};
 
 mod camera;
-mod colour_map;
 mod configuration;
-mod cube;
 mod hittable;
 mod hittables;
 mod material;
-mod noise;
 mod ray;
-mod rectangle;
 mod sphere;
-mod terrain;
 mod triangle;
 mod vec3;
 
@@ -71,7 +69,7 @@ fn random_in_hemisphere(normal: vec3::Vec3) -> vec3::Vec3 {
         -in_unit_sphere
     }
 }
-
+/*
 fn create_world(settings: &RaytracerSettings) -> (Hittables, Camera) {
     let camera = camera::Camera::new(
         settings.look_from,
@@ -161,7 +159,7 @@ fn create_world(settings: &RaytracerSettings) -> (Hittables, Camera) {
 
     (world, camera)
 }
-
+*/
 fn ray_color(ray: ray::Ray, world: &hittables::Hittables, depth: i32) -> vec3::Vec3 {
     let mut hit_rec = hittable::HitRecord::new();
     let bias = 0.01;
@@ -203,11 +201,11 @@ fn ray_color(ray: ray::Ray, world: &hittables::Hittables, depth: i32) -> vec3::V
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 struct Work {
     x: usize,
     y: usize,
-    colour: Option<Rgb<u8>>,
+    colour: Option<Vec<u8>>,
 }
 
 fn create_work_list(image_width: i32, image_height: i32, num_cpu: usize) -> Vec<Vec<Work>> {
@@ -255,16 +253,93 @@ fn sample_pixel(
     pixel_color
 }
 
+fn conv_py_vec(vector: Vec<f64>) -> Vec3 {
+    vec3::Vec3::new(vector[0], vector[1], vector[2])
+}
+
+fn parse_ron_material(mat: Vec<String>) -> Material {
+    let material_type = &mat[0];
+    match &material_type[..] {
+        "Lambertian" => material::Material::Lambertian(vec3::Vec3::new(
+            mat[1].parse::<f64>().unwrap(),
+            mat[2].parse::<f64>().unwrap(),
+            mat[3].parse::<f64>().unwrap(),
+        )),
+        "Metal" => material::Material::Metal(
+            vec3::Vec3::new(
+                mat[1].parse::<f64>().unwrap(),
+                mat[2].parse::<f64>().unwrap(),
+                mat[3].parse::<f64>().unwrap(),
+            ),
+            mat[4].parse::<f64>().unwrap(),
+        ),
+        "Mirror" => material::Material::Mirror,
+        "Dielectric" => material::Material::Dielectric(mat[4].parse::<f64>().unwrap()),
+        &_ => {
+            panic!("Unknown material found")
+        }
+    }
+}
+
+fn parse_ron_object(obj: RonObject) -> Box<dyn Hittable + Send + Sync + 'static> {
+    if obj.objtype == "Sphere" {
+        return Box::new(Sphere::new(
+            conv_py_vec(obj.vectors[0].clone()),
+            obj.scalars[0],
+            parse_ron_material(obj.material),
+        ));
+    } else if obj.objtype == "Triangle" {
+        let cull_back = if obj.scalars[0] == 0.0 { false } else { true };
+        return Box::new(Triangle::new(
+            conv_py_vec(obj.vectors[0].clone()),
+            conv_py_vec(obj.vectors[1].clone()),
+            conv_py_vec(obj.vectors[2].clone()),
+            parse_ron_material(obj.material),
+            cull_back,
+        ));
+    }
+    panic!("unknown ron object type.");
+}
+
 pub fn create_image(ron_string: String) -> Vec<Vec<u8>> {
-    let settings = configuration::RaytracerSettings::from_ron(ron_string);
-    let (world, camera) = create_world(&settings);
+    let settings = configuration::RaytracerScene::from_ron(ron_string);
+
+    let camera = camera::Camera::new(
+        conv_py_vec(settings.camera_pos.clone()),
+        conv_py_vec(settings.camera_dir.clone()),
+        conv_py_vec(settings.camera_up.clone()),
+        settings.v_fov,
+        settings.aspect_ratio,
+        settings.aperture,
+        settings.focal_distance,
+    );
+
+    let mut light_objects = vec![];
+    for light in settings.lights.clone() {
+        light_objects.push(Box::new(conv_py_vec(light.clone())));
+    }
+
+    let mut world_objects: Vec<Box<dyn Hittable + Send + Sync + 'static>> = vec![];
+    for obj in settings.objects.clone() {
+        world_objects.push(parse_ron_object(obj.clone()));
+    }
+
+    let world = Hittables {
+        lights: light_objects,
+        hittables: world_objects,
+    };
 
     let now = Instant::now();
     let image = if settings.multithreading {
-        let image_ = Arc::new(Mutex::new(ImageBuffer::new(
-            settings.image_width as u32,
-            settings.image_height as u32,
-        )));
+        let image_ = Arc::new(Mutex::new({
+            let mut x =
+                Vec::with_capacity(settings.image_width as usize * settings.image_height as usize);
+            x.resize(
+                settings.image_width as usize * settings.image_height as usize,
+                vec![0 as u8, 0 as u8, 0 as u8],
+            );
+            x
+        }));
         let world_ = Arc::new(world);
         let camera_ = Arc::new(camera);
         let settings_ = Arc::new(settings);
@@ -309,11 +384,10 @@ pub fn create_image(ron_string: String) -> Vec<Vec<u8>> {
 
                 let mut image_data = scoped_image.lock().unwrap();
                 for final_work in inner_work_vec {
-                    image_data.put_pixel(
-                        final_work.x as u32,
-                        final_work.y as u32,
-                        final_work.colour.unwrap(),
-                    );
+                    let colour = final_work.colour.unwrap().clone();
+                    image_data[(final_work.x as u32
+                        + (final_work.y as u32 * scoped_settings.image_width as u32))
+                        as usize] = colour;
                     //println!("{}:{}:{:#?}\n", final_work.x, final_work.y, final_work.colour.unwrap())
                 }
 
@@ -327,12 +401,15 @@ pub fn create_image(ron_string: String) -> Vec<Vec<u8>> {
 
         let final_val = match image_.lock() {
             Ok(x) => x.clone(),
-            Err(_) => ImageBuffer::new(settings_.image_width as u32, settings_.image_height as u32),
+            Err(_) => {
+                Vec::with_capacity(settings_.image_width as usize * settings_.image_height as usize)
+            }
         };
         final_val
     } else {
         // Single Thread
-        let mut image_ = RgbImage::new(settings.image_width as u32, settings.image_height as u32);
+        let mut image_ =
+            Vec::with_capacity(settings.image_width as usize * settings.image_height as usize);
         let progress_prints = settings.image_width as f64 / 16.0;
         for j in 0..settings.image_height {
             // progress check
@@ -354,11 +431,8 @@ pub fn create_image(ron_string: String) -> Vec<Vec<u8>> {
                     &camera,
                     &world,
                 );
-                image_.put_pixel(
-                    i as u32,
-                    j as u32,
-                    pixel_color.to_rgb(settings.samples_per_pixel),
-                );
+                image_[i as usize * (j as usize * settings.image_width as usize)] =
+                    pixel_color.to_rgb(settings.samples_per_pixel);
             }
         }
         image_
@@ -374,5 +448,5 @@ pub fn create_image(ron_string: String) -> Vec<Vec<u8>> {
         hours, minutes, seconds
     );
 
-    image.into_vec().map(|x| vec![x.to_rgb().channels()])
+    image
 }
